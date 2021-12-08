4640a9b8e3fd4bb4e8d46bdb196e7d33ea506852
taiga07 <taigatani@me.com>
2021-12-08 15:24:58 +0000

    [add]コントローラーとビューの設定

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,32 @@
+class RoomsController < ApplicationController
+  
+  def create
+    @room = Room.create
+    @joincurrentuser = RoomUser.create(user_id: current_user.id, room_id: @room.id)
+    @joinuser = RoomUser.create(join_room_params)
+    redirect_to room_path(@room.id)
+  end
+  
+  def show
+    @room = Room.find(params[:id])
+    if RoomUser.where(user_id: current_user.id, room_id: @room.id).params?
+      @messages = @room.messages
+      @message = Message.new
+      @roomusers = @room.room_users
+    else
+      redirect_back(fallback_location: root_path)
+    end
+  end
+  
+  private
+  
+  def join_room_params
+    params.require(:room_user).permit(:user_id, :room_id).merge(room_id: @room.id)
+  end
+  
+
+
+
+
+
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,22 +4,32 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    
     @currentRoomUser = RoomUser.where(user_id: current_user.id)
     #current_userが既にルームに参加しているか判断
     @receiveUser = RoomUser.where(user_id: @user.id)
     #他の@userがルームに参加しているか判断
-    
-    unless @user.id == current_user.id  #current_userと@userが一致していなければ
-      @currentRoomUser.each do |cu|  #current_userが参加していルームを取り出す
-        @receiveUser.each do |u|  #@userが参加しているルームを取り出す
-          
-    
     @books = @user.books
     @book = Book.new
     @following_users = @user.following_user
     @follower_users = @user.follower_user
+
+    unless @user.id == current_user.id  #current_userと@userが一致していなければ
+      @currentRoomUser.each do |cu|  #current_userが参加しているルームを取り出す
+        @receiveUser.each do |u|  #@userが参加しているルームを取り出す
+          if cu.room_id == u.room_id  #current_userと@userのルームが同じか判断(既にルームが作られているか)
+            @haveRoom = true  #falseの時(同じじゃない時)の条件を記述するために変数に代入
+            @roomId = cu.room_id  #ルームが共通しているcurrent_userと@userに対して変数を指定
+          end
+        end
+      end
+      unless @haveroom  #ルームが同じじゃなければ
+        @room = Room.new  #新しいインスタンスを生成
+        @roomuser = RoomUser.new  #新しいインスタンスを生成
+      end
+    end
+
   end
+
 
   def index
     @users = User.all
